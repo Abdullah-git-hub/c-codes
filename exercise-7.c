4620e3156332fee5003df4bbee4c2486fa57fcff
Abdullah-git-hub <arafat200423@gmail.com>
2022-03-06 23:20:39 +0600

    Adding

--- exercise-7.c
+++ exercise-7.c
@@ -10,7 +10,38 @@
 
 // main function
 int main(){
-    
+    int n = 1;
+    int sum = 0;
+
+    // using while loop ---***---
+
+    // while (n <= 500)
+    // {
+    //     printf("%d\n",n);
+    //     sum += n; // you can use sum = sum + n
+    //     n += 2;
+    // }
+
+
+    // using do-while loop ---***---
+
+    // do
+    // {
+    //     printf("%d\n", n);
+    //     sum += n;
+    //     n += 2;
+    // } while (n <= 500);
+
+
+    // using for loop
+
+    for (n; n <= 500; n += 2)
+    {
+        printf("%d\n", n);
+        sum += n;
+    }
+
+    printf("The sum is %d \n", sum);
 
     return 0;
 }
